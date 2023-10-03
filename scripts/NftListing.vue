d0945451473ba080374b47496983457e8dd6d2e1
MMCCSolana <114774208+MMCCSolana@users.noreply.github.com>
2023-10-02 22:17:08 -0500

    Initial commit

---
+++ scripts/NftListing.vue
@@ -0,0 +1,20 @@
+
+    <template>
+      <div class="nft-listing">
+        <!-- Your HTML for the listing goes here -->
+      </div>
+    </template>
+
+    <script>
+    export default {
+      name: 'NftListing',
+      props: {
+        nft: Object
+      }
+    }
+    </script>
+
+    <style scoped>
+    /* Your CSS styles go here */
+    </style>
+    
---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+
+    import Vue from 'vue';
+    import NftListing from './NftListing.vue';
+
+    new Vue({
+      el: '#app',
+      components: {
+        NftListing
+      }
+    });
+    
