34ebf9e5499725ff9a3f84f08b5bb95c7c6ef4ee
MMCCSolana <114774208+MMCCSolana@users.noreply.github.com>
2023-10-02 22:23:36 -0500

    update

--- scripts/NftListing.vue
+++ scripts/NftListing.vue
@@ -1,20 +1,28 @@
 
-    <template>
-      <div class="nft-listing">
-        <!-- Your HTML for the listing goes here -->
-      </div>
-    </template>
+<template>
+  <div class="nft-listing">
+    <img :src="nft.image" alt="NFT Image" width="100">
+    <h2>{{ nft.name }}</h2>
+    <p>{{ nft.inscription }}</p>
+    <p>Price: {{ nft.price }}</p>
+    <a :href="nft.viewLink">View</a>
+    <a :href="nft.buyLink">Buy</a>
+  </div>
+</template>
 
-    <script>
-    export default {
-      name: 'NftListing',
-      props: {
-        nft: Object
-      }
-    }
-    </script>
+<script>
+export default {
+  name: 'NftListing',
+  props: {
+    nft: Object
+  }
+}
+</script>
 
-    <style scoped>
-    /* Your CSS styles go here */
-    </style>
-    
+<style scoped>
+.nft-listing {
+  border: 1px solid #ddd;
+  padding: 10px;
+  margin: 10px;
+}
+</style>
--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,16 @@
 
-    import Vue from 'vue';
-    import NftListing from './NftListing.vue';
+import Vue from 'vue';
+import NftListing from './NftListing.vue';
 
-    new Vue({
-      el: '#app',
-      components: {
-        NftListing
-      }
-    });
-    
+// Assuming data is globally available, for simplicity in this mockup
+const nfts = window.nftData;
+
+new Vue({
+  el: '#app',
+  data: {
+    nfts: nfts
+  },
+  components: {
+    NftListing
+  }
+});
